e5b4c3bf587c53ff3e6b7e752e0c60580847a5c7
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-09-30 22:12:20 -0500

    all data types
    number, bigint, string, boolean, undefined, object, symbol

--- datatypesscript.js
+++ datatypesscript.js
@@ -4,3 +4,34 @@ console.log(typeof x)
 //big int
 var y=10n
 console.log(typeof y)
+
+//string 
+var name2='nikhil2'
+console.log(typeof name2)
+
+//boolean
+var isMonday = true
+console.log(typeof isMonday)
+
+//undefined
+var z
+console.log(typeof z)
+
+//objects
+var obj={}
+console.log(typeof obj)
+
+var arr=[]
+console.log(typeof arr)
+
+//symbol
+var sym = Symbol("id")
+console.log(typeof sym)
+
+//null
+var x1 = 10
+x1 = null
+console.log(typeof x1)
+
+// string() boolean()
+console.log(typeof new String("hey"))
