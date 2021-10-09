a11d37c3fd62663446108eef8f601f863345a9b7
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-09 11:33:33 -0500

    generate random number
    generate random numbers when mouse moves

--- events.js
+++ events.js
@@ -1,12 +1,20 @@
 
-// //manual click of button function
-function firstFunction(){
-    console.log("this is strange676")
-}
+// // //manual click of button function
+// function firstFunction(){
+//     console.log("this is strange676")
+// }
     
 
-// //programmatic click
-let btn = document.querySelector("button")
-btn.addEventListener("click",firstFunction)
+// // //programmatic click
+// let btn = document.querySelector("button")
+// btn.addEventListener("click",firstFunction)
+
+//generate random numbers when the mouse moves in HTML page
+document.addEventListener("mousemove",handler)
+function handler(){
+    document.querySelector(".demo").innerHTML = Math.random()
+}
+
+
 
 
