a575bb4d8ea8769ef980f328a90987209a64ec6e
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-09 12:13:01 -0500

    event bubbling alerts
    alert message in html page. if p is clicked, alerts for p, div and form is displayed.
    
    if div is clicked, alert message for div and form is displayed.
    
    If form is clicked, only form alert is displayed.

--- events.js
+++ events.js
@@ -9,16 +9,18 @@
 // let btn = document.querySelector("button")
 // btn.addEventListener("click",firstFunction)
 
-//generate random numbers when the mouse moves in HTML page
-document.addEventListener("mousemove",handler)
-function handler(){
-    document.querySelector(".demo").innerHTML = Math.random()
-}
+// generate random numbers when the mouse moves in HTML page
+// document.addEventListener("mousemove",handler)
+// function handler(){
+//     document.querySelector(".demo").innerHTML = Math.random()
+// }
 
-//remove handler to stop the random number generation
-function removeHandler(){
-    document.removeEventListener("mousemove",handler)
-}
+// //remove handler to stop the random number generation
+// function removeHandler(){
+//     document.removeEventListener("mousemove",handler)
+// }
+
+
 
 
 
