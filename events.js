d87ea5840bb4cf41b09766c59cc754631f22e2bd
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-09 11:25:12 -0500

    event listener- programmatic click
    programmatic click with eventlistener

--- events.js
+++ events.js
@@ -1,4 +1,12 @@
+
+// //manual click of button function
 function firstFunction(){
-    console.log("Hurray")
+    console.log("this is strange676")
 }
     
+
+// //programmatic click
+let btn = document.querySelector("button")
+btn.addEventListener("click",firstFunction)
+
+
