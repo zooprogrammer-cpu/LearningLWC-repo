08f437488a867158942a2c1895ff76f49e131bb6
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-09 17:20:19 -0500

    arrow functions
    arrow functions to add two numbers

---
+++ arrow.js
@@ -0,0 +1,31 @@
+//Normal  Function 
+// function abc(){
+//     console.log("hello")
+// }
+
+// abc()
+
+
+//Arrow Function 
+
+
+const abc = ()=>console.log ("hello")
+abc()
+
+//sum example with one data
+
+const sum = (data)=>{
+    let sum = data + 10
+    return sum
+}
+console.log(sum(4))
+
+//sum example with two data
+const sum2 = (data1,data2)=>{
+    let sum2 = data1 + data2
+    return sum2
+}
+console.log(sum2(4,5))
+
+
+
